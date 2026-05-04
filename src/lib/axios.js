5f91a0e94a9be9b222eb8c64ef334af0b3f8bcc3
utsav8196-del <utsav8196@gmail.com>
2026-05-04 18:48:30 +0530

    ccc

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api";
-// const BASE_URL = "http://localhost:8000/api";
-// const BASE_URL = import.meta.env.VITE_BASE_URL || "https://stackchatbackend.vercel.app";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || (import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api");
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
